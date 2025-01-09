880341b35dd45103d53fc6e954115ec7bbbfb6c3
Joshua Williams <joshuaw@hpe.com>
2025-01-09 07:05:46 -0800

    CASMHMS-6324: hmnfd pprof path should be v2 and not v1

--- cmd/hmi-nfd/pprof.go
+++ cmd/hmi-nfd/pprof.go
@@ -36,19 +36,19 @@ import (
 
 func RegisterPProfHandlers(router *mux.Router) {
 	// Main profiling entry point
-	router.HandleFunc("/hmi/v1/debug/pprof/", pprof.Index) // Index listing all pprof endpoints
+	router.HandleFunc("/hmi/v2/debug/pprof/", pprof.Index) // Index listing all pprof endpoints
 
 	// Specific profiling handlers
-	router.HandleFunc("/hmi/v1/debug/pprof/cmdline", pprof.Cmdline) // Command-line arguments
-	router.HandleFunc("/hmi/v1/debug/pprof/profile", pprof.Profile) // CPU profile (default: 30 seconds)
-	router.HandleFunc("/hmi/v1/debug/pprof/symbol", pprof.Symbol)   // Symbol resolution for addresses
-	router.HandleFunc("/hmi/v1/debug/pprof/trace", pprof.Trace)     // Execution trace (default: 1 second)
+	router.HandleFunc("/hmi/v2/debug/pprof/cmdline", pprof.Cmdline) // Command-line arguments
+	router.HandleFunc("/hmi/v2/debug/pprof/profile", pprof.Profile) // CPU profile (default: 30 seconds)
+	router.HandleFunc("/hmi/v2/debug/pprof/symbol", pprof.Symbol)   // Symbol resolution for addresses
+	router.HandleFunc("/hmi/v2/debug/pprof/trace", pprof.Trace)     // Execution trace (default: 1 second)
 
 	// Additional profiling endpoints
-	router.Handle("/hmi/v1/debug/pprof/allocs", pprof.Handler("allocs"))             // Heap allocation samples
-	router.Handle("/hmi/v1/debug/pprof/block", pprof.Handler("block"))               // Goroutine blocking events
-	router.Handle("/hmi/v1/debug/pprof/goroutine", pprof.Handler("goroutine"))       // Stack traces of all goroutines
-	router.Handle("/hmi/v1/debug/pprof/heap", pprof.Handler("heap"))                 // Memory heap profile
-	router.Handle("/hmi/v1/debug/pprof/mutex", pprof.Handler("mutex"))               // Mutex contention profile
-	router.Handle("/hmi/v1/debug/pprof/threadcreate", pprof.Handler("threadcreate")) // Stack traces of thread creation
+	router.Handle("/hmi/v2/debug/pprof/allocs", pprof.Handler("allocs"))             // Heap allocation samples
+	router.Handle("/hmi/v2/debug/pprof/block", pprof.Handler("block"))               // Goroutine blocking events
+	router.Handle("/hmi/v2/debug/pprof/goroutine", pprof.Handler("goroutine"))       // Stack traces of all goroutines
+	router.Handle("/hmi/v2/debug/pprof/heap", pprof.Handler("heap"))                 // Memory heap profile
+	router.Handle("/hmi/v2/debug/pprof/mutex", pprof.Handler("mutex"))               // Mutex contention profile
+	router.Handle("/hmi/v2/debug/pprof/threadcreate", pprof.Handler("threadcreate")) // Stack traces of thread creation
 }
